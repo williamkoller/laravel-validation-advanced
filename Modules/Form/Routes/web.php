e17f7b5541b2065a3e529cc87dfaee83df491158
William Koller <wkoller25@gmail.com>
2019-06-27 14:42:28 -0300

    em desenvolvimento

--- Modules/Form/Routes/web.php
+++ Modules/Form/Routes/web.php
@@ -15,18 +15,21 @@
 //     Route::get('/', 'FormController@index');
 // });
 
-Route::group([
-
-], function () {
-   Route::get('admin/clients', ['as' => 'admin.clients.listAll', 'uses' => 'FormController@listAll']);
-   Route::get('admin/clients/edit/{id}', ['as' => 'admin.clients.editClients', 'uses' => 'FormController@editClients']);
-   Route::get('admin/clients/create', ['as' => 'admin.clients.createClients', 'uses' => 'FormController@createClients']);
-   Route::post('admin/clients/store', ['as' => 'admin.clients.store', 'uses' => 'FormController@store']);
-});
-
 //Route::group([
-//    'prefix' => 'admin'
 //
 //], function () {
-//   Route::resource('clients', 'FormController');
+//   Route::get('admin/clients', ['as' => 'admin.clients.listAll', 'uses' => 'FormController@listAll']);
+//   Route::get('admin/clients/edit', ['as' => 'admin.clients.editClients', 'uses' => 'FormController@editClients']);
+//   Route::get('admin/clients/create', ['as' => 'admin.clients.createClients', 'uses' => 'FormController@createClients']);
+//   Route::post('admin/clients/store', ['as' => 'admin.clients.store', 'uses' => 'FormController@store']);
+//   Route::get('admin/clients/update', ['as' => 'admin.clients.updateClients', 'uses' => 'FormController@updateClients']);
+//   Route::get('admin/clients/show', ['as' => 'admin.clients.showClients', 'uses' => 'FormController@showClients']);
+//   Route::delete('admin/clients/{id}', ['as' => 'admin.clients.destroyClients', 'uses' => 'FormController@destroyClients']);
 //});
+
+Route::group([
+    'prefix' => 'admin'
+
+], function () {
+   Route::resource('clients', 'FormController');
+});

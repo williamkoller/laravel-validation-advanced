cdd756aa6df9586c1e8a4aedb75b62215dde6717
William Koller <wkoller25@gmail.com>
2019-08-23 15:31:49 -0300

    fixit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                    <a href="{{ route('admin.clients') }}">Clients</a>
+                    <a href="{{ route('clients.index') }}">List</a>
                 </div>
             </div>
         </div>

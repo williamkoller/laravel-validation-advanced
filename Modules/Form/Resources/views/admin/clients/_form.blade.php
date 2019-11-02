c3b8e3835f63df356ae868cae33b547d9096024c
William Koller <wkoller25@gmail.com>
2019-11-02 15:21:28 -0300

    alter message for english

--- Modules/Form/Resources/views/admin/clients/_form.blade.php
+++ Modules/Form/Resources/views/admin/clients/_form.blade.php
@@ -2,14 +2,14 @@
 {{ Form::hidden('client_type', $clientType) }}
 {{--<input type="hidden" name="client_type" value="{{ $clientType }}">--}}
 @component('form::form._form_group',['field' => 'name'])
-    {{ Form::label('name', 'Nome',['class' => 'control-label']) }}
+    {{ Form::label('name', 'Name',['class' => 'control-label']) }}
     {{ Form::text('name',null,['class' => 'form-control']) }}
 {{--    <label for="name">Name</label>--}}
 {{--    <input class="form-control" id="name" name="name" value="{{ old('name', $client->name) }}">--}}
 
 @endcomponent
 @component('form::form._form_group',['field' => 'document_number'])
-    {{ Form::label('document_number', 'Documento',['class' => 'control-label']) }}
+    {{ Form::label('document_number', 'Document',['class' => 'control-label']) }}
     {{ Form::text('document_number',null,['class' => 'form-control']) }}
 {{--    <label for="document_number">Document</label>--}}
 {{--    <input class="form-control" id="document_number" name="document_number" value="{{ old('document_number', $client->document_number) }}">--}}
@@ -21,7 +21,7 @@
 {{--    <input class="form-control" id="email" name="email" value="{{ old('email', $client->email) }}">--}}
 @endcomponent
 @component('form::form._form_group',['field' => 'phone'])
-    {{ Form::label('phone', 'Telefone',['class' => 'control-label']) }}
+    {{ Form::label('phone', 'Phone',['class' => 'control-label']) }}
     {{ Form::text('phone',null,['class' => 'form-control']) }}
 {{--    <label for="phone">Phone</label>--}}
 {{--    <input class="form-control" id="phone" name="phone" value="{{ old( 'phone', $client->phone) }}">--}}
@@ -31,13 +31,13 @@
         $maritalStatus = $client->marital_status;
     @endphp
 @component('form::form._form_group',['field' => 'marital_status'])
-        {{ Form::label('marital_status', 'Estado Cívil',['class' => 'control-label']) }}
+        {{ Form::label('marital_status', 'Marital Status',['class' => 'control-label']) }}
 {{--        <label for="marital_status">Marital Status</label>--}}
         {{  Form::select('marital_status', [
-        '' => 'Selecione o estado cívil',
-         1 => 'Solteiro',
-         2 => 'Casado',
-         3 => 'Divorciado',
+        '' => 'Select Marital Status',
+         1 => 'Single',
+         2 => 'Married',
+         3 => 'Divorced',
         ], null, ['class' => 'form-control'])
         }}
 {{--        <select class="form-control" name="marital_status" id="marital_status" value="{{ $maritalStatus }}">--}}
@@ -50,7 +50,7 @@
 @endcomponent
 
 @component('form::form._form_group',['field' => 'date_birth'])
-        {{ Form::label('date_birth', 'Data Nasc',['class' => 'control-label']) }}
+        {{ Form::label('date_birth', 'Date Birth',['class' => 'control-label']) }}
         {{ Form::date('date_birth', null,['class' => 'form-control']) }}
 {{--        <label for="date_birth">Date Birth</label>--}}
 {{--        <input class="form-control" id="date_birth" name="date_birth" type="date" value="{{ old('date_birth', $client->date_birth) }}">--}}
@@ -61,14 +61,14 @@
     @endphp
     <div class="radio {{$errors->has('sex')?' has-danger':''}}">
         <label>
-            {{ Form::radio('sex', 'm') }} Masculino
+            {{ Form::radio('sex', 'm') }} Male
 {{--            <input type="radio" name="sex" value="m" {{ old('sex', $sex) == 'm'?'checked="checked"' : ''}}> Male--}}
         </label>
     </div>
 
     <div class="radio {{$errors->has('sex'?' has-danger':'')}}">
         <label>
-            {{ Form::radio('sex', 'f') }} Feminino
+            {{ Form::radio('sex', 'f') }} Female
 {{--            <input type="radio" name="sex" value="f" {{ old('sex', $sex) == 'f'?'checked="checked"' : ''}}> Female--}}
         </label>
     </div>
@@ -77,14 +77,14 @@
 
     </div>
     @component('form::form._form_group',['field' => 'physical_disability'])
-        {{ Form::label('physical_disability', 'Deficiência Física',['class' => 'control-label']) }}
+        {{ Form::label('physical_disability', 'Physical Disability',['class' => 'control-label']) }}
         {{ Form::text('physical_disability', null,['class' => 'form-control']) }}
 {{--        <label for="physical_disability">Physical Disability</label>--}}
 {{--        <input class="form-control" id="physical_disability" name="physical_disability" value="{{ old('physical_disability', $client->physical_disability) }}">--}}
     @endcomponent
     @else
     @component('form::form._form_group',['field' => 'company_name'])
-            {{ Form::label('company_name', 'Nome Fantasia',['class' => 'control-label']) }}
+            {{ Form::label('company_name', 'Company Name',['class' => 'control-label']) }}
             {{ Form::text('company_name', null,['class' => 'form-control']) }}
 {{--            <label for="company_name">Company Name</label>--}}
 {{--            <input class="form-control" id="company_name" name="company_name" value="{{ old('company_name', $client->company_name) }}">--}}
@@ -93,7 +93,7 @@
 
 <div class="checkbox">
     <label>
-        {{ Form::checkbox('defaulter') }} Inadimplente?
+        {{ Form::checkbox('defaulter') }} Defaulting?
         {{--        <input id="defaulter" name="defaulter" type="checkbox" {{ old('defaulter', $client->defaulter) ? 'checked="checked"' : '' }}> Defaulter?--}}
     </label>
 </div>
